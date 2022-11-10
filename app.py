948c3e5d6da34aed20014e486c4039b9384f5115
dylnbk <dyln.bk@gmail.com>
2022-11-10 15:04:34 +0000

    Layout & code refined

--- app.py
+++ app.py
@@ -23,6 +23,17 @@ def progress_func(stream, chunk, bytes_remaining):
     p = int((float(abs(bytes_remaining-size)/size))*float(100))
     bar.progress(p)
 
+# YouTube downloader
+def youtube_download():
+
+    # grab YouTube datastream, on_progress_callback generates progress bar data
+    yt = YouTube(url_from_user, on_progress_callback=progress_func)
+    stream = yt.streams.get_by_itag(22)
+    
+    # generate option to download file
+    with open(stream.download(), "rb") as file:
+        st.download_button("Download", data=file, file_name="grabit.mp4", mime="video")
+
 # main
 local_css("style.css")
 st.title('Grab it.')
@@ -30,7 +41,7 @@ st.title('Grab it.')
 # create a form to capture URL and take user options
 with st.form("input", clear_on_submit=True):
 
-    url_from_user = st.text_input('Enter the YouTube link:', placeholder='https://www.youtube.com/watch...')
+    url_from_user = st.text_input('Enter the link:', placeholder='https://www.your-link-here.com/...')
 
     # create a column layout for the selectbox and submit button
     col1, col2 = st.columns([6.5, 1])
@@ -40,22 +51,13 @@ with st.form("input", clear_on_submit=True):
     with col2:
         confirm_selection = st.form_submit_button("Submit")
 
-# once the user hits submit
 if confirm_selection:
 
-    # if the user input is complete
-    if url_from_user:
+    # initialize a progress bar
+    bar = st.progress(3)
+    
+    # display users video
+    st.video(url_from_user)
 
-        # initialize a progress bar
-        bar = st.progress(0)
-
-        # grab YouTube datastream
-        yt = YouTube(url_from_user, on_progress_callback=progress_func)
-        stream = yt.streams.get_by_itag(22)
-
-        # display users video
-        st.video(url_from_user)
-
-        # generate option to download file
-        with open(stream.download(), "rb") as file:
-            st.download_button("Download", data=file, file_name="grabit.mp4", mime="video")
+    # grab content and generate download button
+    youtube_download()
